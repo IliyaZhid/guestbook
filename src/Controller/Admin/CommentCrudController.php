101a9d017fddd48457c2d389d22018c07cf297d2
Iliya Zhidkikh <iliyazhid@mail.ru>
2024-12-10 19:11:06 +0700

    Определение коллбэков жизненного цикла

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -51,11 +51,9 @@ class CommentCrudController extends AbstractCrudController
             'years' => range(date('Y'),date('Y') + 5),
             'widget' => 'single_text',
         ]);
-        if (Crud::PAGE_EDIT === $pageName) {
+        if (Crud::PAGE_EDIT === $pageName)
             yield $createdAt->setFormTypeOption('disabled', true);
-        } else {
-            yield $createdAt;
-        }
+
     }
 
 }
